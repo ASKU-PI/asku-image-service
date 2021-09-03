d5f541ae575e7292d84c001a8240bc20f1dffcd6
Szymon Świerk <swierk.szymon99@gmail.com>
2021-09-04 01:33:58 +0200

    basic implementation

---
+++ models/magazine.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const Magazine = new Schema ({
+    id: { type: Number, required: true },
+    photos: [{url: String}]
+});
+
+module.exports = mongoose.model('Magazine', Magazine);
---
+++ models/profile.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const Profile = new Schema ({
+    id: { type: String, required: true },
+    photo: { url: String }
+});
+
+module.exports = mongoose.model('Profile', Profile);
