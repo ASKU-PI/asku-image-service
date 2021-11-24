40cfcb87a29e03adcd681eb5e69a4c71a8d2524d
Vlad Tumilovich <vladislav.tumilovich@gmail.com>
2021-11-24 21:40:47 +0100

    Fix code accordingly to ci check

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const eurekaClient = require("./config/eureka");
-const uploader = require("./utils/uploader");
-require("./config/mongo");
+const express = require('express');
+const eurekaClient = require('./config/eureka');
+const uploader = require('./utils/uploader');
+require('./config/mongo');
 
 const app = express();
 const port = 8892;
 
-const profileController = require("./controllers/profile_controller");
-const magazineController = require("./controllers/magazine_controller");
+const profileController = require('./controllers/profile_controller');
+const magazineController = require('./controllers/magazine_controller');
 
 eurekaClient.start();
 
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'));
 
 app.post(
-  "/profile",
-  uploader.single("picture"),
-  profileController.createOrUpdateProfilePicture
+  '/profile',
+  uploader.single('picture'),
+  profileController.createOrUpdateProfilePicture,
 );
 
-app.get("/profile", profileController.getProfilePicture);
+app.get('/profile', profileController.getProfilePicture);
 
-app.delete("/profile", profileController.deleteProfilePicture);
+app.delete('/profile', profileController.deleteProfilePicture);
 
 app.post(
-  "/magazine",
-  uploader.array("picture", 20),
-  magazineController.addMagazinePhotos
+  '/magazine',
+  uploader.array('picture', 20),
+  magazineController.addMagazinePhotos,
 );
 
-app.get("/magazine", magazineController.getMagazinePhotos);
+app.get('/magazine', magazineController.getMagazinePhotos);
 
-app.delete("/magazine", magazineController.deleteMagazinePhoto);
+app.delete('/magazine', magazineController.deleteMagazinePhoto);
 
 app.listen(port, () => {
   console.log(`ASKU Image Service listening at http://localhost:${port}`);
